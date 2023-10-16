a3e64901a4ca91313ad120bfc7a8d7716abb1da2
smndtrl <smndtrl@users.noreply.github.com>
2023-10-16 15:52:14 +0200

    add second page nav

--- app/not-home/page.tsx
+++ app/not-home/page.tsx
@@ -6,121 +6,12 @@ import Link from "next/link"
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
 
       <Link href='#section5' rel="noopener">
         Test
       </Link>
-      <Link href='/not-home' rel="noopener">
-        Link to Top on NotHome
-      </Link>
-      <Link href='/not-home#section5' rel="noopener">
-        Link to Section on NotHome
-      </Link>
+
       <p>
 
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed egestas mi et quam euismod luctus. Praesent mattis vehicula nibh non luctus. Curabitur non nisi sapien. In at risus non justo facilisis porttitor efficitur id urna. Aliquam erat volutpat. Duis metus mauris, dictum ut leo quis, laoreet aliquet lorem. Integer eleifend mauris vel metus commodo condimentum. Quisque volutpat molestie scelerisque. Fusce tempus eros eu iaculis tempus. Vivamus at velit at felis tristique vehicula. Curabitur ultrices viverra purus, nec elementum lacus ultricies quis. Aliquam volutpat felis massa, vitae varius urna aliquet et. Nullam accumsan ligula sit amet nunc hendrerit, non hendrerit dui tempus. Nam orci mauris, commodo sed iaculis sed, iaculis ut ante.
@@ -165,8 +56,11 @@ export default function Home() {
         <a id="section5" />
         Integer aliquam dolor eleifend erat blandit, id imperdiet nulla facilisis. Suspendisse dignissim lorem a arcu mollis, sit amet pulvinar tortor dapibus. Maecenas sagittis, magna id sodales posuere, metus purus malesuada nunc, efficitur pretium ex arcu id sem. Vestibulum metus elit, sagittis sit amet pellentesque at, ullamcorper ut orci. Quisque at suscipit libero. Nam hendrerit purus ac dignissim laoreet. Maecenas est orci, semper sit amet vehicula eu, vestibulum feugiat neque. Quisque ullamcorper mattis odio, nec hendrerit libero luctus eget. Pellentesque fermentum enim non mauris semper commodo. In semper maximus ante, et euismod magna aliquet non. Aenean a volutpat nunc. Donec eu ante mattis, commodo nibh a, facilisis purus. Maecenas vitae neque vel dolor venenatis sodales eu ut massa.
       </p>
-      <Link href='' rel="noopener">
-        Back
+      <Link href='#' rel="noopener">
+        Back to the top
+      </Link>
+      <Link href='/' rel="noopener">
+        Back to Home
       </Link>
     </main>
   )
